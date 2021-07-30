e923e884858e74c559c18725c54561842a2a2305
NRPonugoti <narendra.ponugoti@gmail.com>
2021-07-30 15:40:46 +0200

    First Commit

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name= "Narendra";
+		if(name=="Narendra") {
+			System.out.println("Name is Narendra");
+		} else {
+			System.out.println(" Name is incorrect ");
+		}
+
+	}
+
+}
